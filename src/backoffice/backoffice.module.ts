8df4269b5fd85cb7fc40ddfe4d7b4fb4ed8cb4de
Bruno Andrade <brunnoandradi@gmail.com>
2021-05-11 22:25:52 -0300

    create user controller

--- src/backoffice/backoffice.module.ts
+++ src/backoffice/backoffice.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { UserController } from 'src/controllers/user.controller';
 
-@Module({})
+@Module({
+    controllers: [UserController]
+})
 export class BackofficeModule {}
