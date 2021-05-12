6d712ec86531c9bad36f9fa7b5e5f8da6fbaef09
Bruno Andrade <brunnoandradi@gmail.com>
2021-05-12 09:48:38 -0300

    create routes user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,19 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
-@Controller()
+// localhost:3000/users
+@Controller('v1/users')
 export class UserController {
+    @Get()
     get() {
         return 'Obter os usuários';
     }
 
+    @Post()
     post() {
         return 'Criar um usuário';
     }
 
+    @Put()
     put() {
         return 'Atualizar um usuário';
     }
 
+    @Delete()
     delete() {
         return 'Remover um usuário';
     }
