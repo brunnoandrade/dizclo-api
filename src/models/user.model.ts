b32b89fe0436c0666691113d2a1406203746a0c6
Bruno Andrade <brunnoandradi@gmail.com>
2021-05-13 13:51:30 -0300

    create models pet, address and credit-card

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,9 +1,17 @@
+import { Pet } from "./pet.model";
+import { Address } from "./address.model";
+import { CreditCard } from "./credit-card.model";
+
 export class User {
     constructor( 
         public name: string,
         public document: string,
         public email: string,
         public password: string,
+        public pets: Pet[],
+        public billingAddress: Address,
+        public shippingAddress: Address,
+        public creditCard: CreditCard,
         public active: boolean
     ) {
        
