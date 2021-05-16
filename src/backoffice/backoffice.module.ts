91ac481e0b463c2cb7a7b5b0b8fe83f97ccde296
Bruno Andrade <brunnoandradi@gmail.com>
2021-05-16 16:57:24 -0300

    connect to mongodb

--- src/backoffice/backoffice.module.ts
+++ src/backoffice/backoffice.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { CustomerSchema } from './schemas/customer.schema';
+import { UserSchema } from './schemas/user.schema';
+
 import { CustomerController } from './controllers/customer.controller';
 
 @Module({
-    controllers: [CustomerController]
+    imports: [
+        MongooseModule.forFeature([
+            {
+                name: 'Customer',
+                schema: CustomerSchema,
+            },
+            {
+                name: 'User',
+                schema: UserSchema,
+            },
+        ]),
+    ],
+    controllers: [CustomerController],
+
 })
 export class BackofficeModule {}
