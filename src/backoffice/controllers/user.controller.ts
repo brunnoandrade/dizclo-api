26f9dae37b88eec686681d21bf798de268eb2a72
Bruno Andrade <brunnoandradi@gmail.com>
2021-05-16 16:07:04 -0300

    implement user interceptor in user controller

--- src/backoffice/controllers/user.controller.ts
+++ src/backoffice/controllers/user.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Delete, Get, Post, Put, Param, Body } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put, Param, Body, UseInterceptors } from '@nestjs/common';
+import { ValidatorInterceptor } from 'src/interceptors/validator.interceptor';
+import { CreateUserContract } from '../contracts/user.contracts';
 import { Result } from '../models/result.model';
 import { User } from '../models/user.model';
 
@@ -16,6 +18,7 @@ export class UserController {
     }
 
     @Post()
+    @UseInterceptors(new ValidatorInterceptor(new CreateUserContract()))
     post(@Body() body: User) {
         return new Result('Cliente criado com sucesso!', true, body, null)
     }
