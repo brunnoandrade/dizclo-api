beba51c34161638395276ba732831194e72a359a
Bruno Andrade <brunnoandradi@gmail.com>
2021-05-17 20:24:10 -0300

    create add billing address in customer

--- src/backoffice/services/customer.service.ts
+++ src/backoffice/services/customer.service.ts
@@ -2,15 +2,25 @@ import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Customer } from '../models/customer.model';
+import { Address } from '../models/address.model';
 
 @Injectable()
 export class CustomerService {
     constructor(
-        @InjectModel('Customer') private readonly customerModel: Model<Customer>
+        @InjectModel('Customer') private readonly model: Model<Customer>
     ) { }
 
     async create(data: Customer): Promise<Customer> {
-        const customer = new this.customerModel(data);
+        const customer = new this.model(data);
         return await customer.save();
+    }
+
+    async addBillingAddress(document: string, data: Address): Promise<Customer> {
+        const options = { upsert: true };
+        return await this.model.findOneAndUpdate({ document }, {
+            $set: {
+                billingAddress: data
+            }
+        }, options);
     }
 }
