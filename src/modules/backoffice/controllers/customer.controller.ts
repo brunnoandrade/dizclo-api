f37af6d29f8962cfccfedcbd16901ca9a2d735fd
Bruno Andrade <brunnoandradi@gmail.com>
2021-05-18 00:42:53 -0300

    add credit card

--- src/modules/backoffice/controllers/customer.controller.ts
+++ src/modules/backoffice/controllers/customer.controller.ts
@@ -2,15 +2,17 @@ import { Controller, Get, Post, Put, Param, Body, UseInterceptors, HttpException
 import { ValidatorInterceptor } from 'src/interceptors/validator.interceptor';
 
 import { CreateCustomerContract } from 'src/modules/backoffice/contracts/customer/create-customer.contract';
-import { UpdateCustomerContract } from '../contracts/customer/update-customer.contract';
+import { UpdateCustomerContract } from 'src/modules/backoffice/contracts/customer/update-customer.contract';
+import { CreateCreditCardContract } from 'src/modules/backoffice/contracts/customer/create-credit-card.contract';
 import { QueryContract } from 'src/modules/backoffice/contracts/query.contract';
 
-import { Result } from 'src/modules/backoffice/models/result.model';
 import { User } from 'src/modules/backoffice/models/user.model';
 import { Customer } from 'src/modules/backoffice/models/customer.model';
+import { CreditCard } from 'src/modules/backoffice/models/credit-card.model';
+import { Result } from 'src/modules/backoffice/models/result.model';
 
 import { CreateCustomerDto } from 'src/modules/backoffice/dtos/customer/create-customer.dto';
-import { UpdateCustomerDto } from '../dtos/customer/update-customer.dto';
+import { UpdateCustomerDto } from 'src/modules/backoffice/dtos/customer/update-customer.dto';
 import { QueryDto } from 'src/modules/backoffice/dtos/query.dto';
 
 import { CustomerService } from 'src/modules/backoffice/services/customer.service';
@@ -68,6 +70,17 @@ export class CustomerController {
     async query(@Body() model: QueryDto) {
         const customers = await this.customerService.query(model);
         return new Result(null, true, customers, null);
+    }
+
+    @Post(':document/credit-cards')
+    @UseInterceptors(new ValidatorInterceptor(new CreateCreditCardContract()))
+    async createCreditCard(@Param('document') document, @Body() model: CreditCard) {
+        try {
+            await this.customerService.saveOrUpdateCreditCard(document, model);
+            return new Result(null, true, model, null);
+        } catch (error) {
+            throw new HttpException(new Result('Não foi possível adicionar seu cartão de crédito', false, null, error), HttpStatus.BAD_REQUEST);
+        }
     }
 
 }
