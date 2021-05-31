833c952cde8291b72dac98ae7d507374b260a3ee
Bruno Andrade <brunnoandradi@gmail.com>
2021-05-31 13:43:09 -0300

    create partner

--- src/modules/backoffice/backoffice.module.ts
+++ src/modules/backoffice/backoffice.module.ts
@@ -8,16 +8,19 @@ dotenv.config();
 import { JwtStrategy } from 'src/shared/strategies/jwt.strategy';
 
 import { CustomerSchema } from 'src/modules/backoffice/schemas/customer.schema';
+import { PartnerSchema } from 'src/modules/backoffice/schemas/partner.schema';
 import { UserSchema } from 'src/modules/backoffice/schemas/user.schema';
 
 import { AccountService } from 'src/modules/backoffice/services/account.service';
 import { CustomerService } from 'src/modules/backoffice/services/customer.service';
+import { PartnerService } from 'src/modules/backoffice/services/partner.service';
 import { AddressService } from 'src/modules/backoffice/services/address.service';
 import { PetService } from 'src/modules/backoffice/services/pet.service';
 import { AuthService } from 'src/shared/services/auth.service';
 
 import { AccountController } from 'src/modules/backoffice/controllers/account.controller';
 import { CustomerController } from 'src/modules/backoffice/controllers/customer.controller';
+import { PartnerController } from 'src/modules/backoffice/controllers/partner.controller';
 import { AddressController } from 'src/modules/backoffice/controllers/address.controller';
 import { PetController } from 'src/modules/backoffice/controllers/pet.controller';
 
@@ -39,13 +42,17 @@ import { PetController } from 'src/modules/backoffice/controllers/pet.controller
                     schema: CustomerSchema,
                 },
                 {
+                    name: 'Partner',
+                    schema: PartnerSchema,
+                },
+                {
                     name: 'User',
                     schema: UserSchema,
                 },
             ]
         ),
     ],
-    controllers: [AccountController, CustomerController, AddressController, PetController],
-    providers: [AccountService, CustomerService, AddressService, PetService, AuthService, JwtStrategy]
+    controllers: [AccountController, CustomerController, PartnerController, AddressController, PetController],
+    providers: [AccountService, CustomerService, PartnerService, AddressService, PetService, AuthService, JwtStrategy]
 })
 export class BackofficeModule {}
